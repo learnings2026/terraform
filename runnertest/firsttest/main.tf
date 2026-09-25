99035da3192fcd8fc1647f63e4e4cd9633be0f47
maklearnings <mak.boopathi@gmail.com>
2026-09-25 16:34:06 +0800

    Rename security group and update references

--- runnertest/firsttest/main.tf
+++ runnertest/firsttest/main.tf
@@ -18,8 +18,8 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_security_group" "ec2_sg" {
-  name        = "terraform-ec2-sg"
+resource "aws_security_group" "ec2_sg1" {
+  name        = "terraform-ec2-sg1"
   description = "Security group for Terraform EC2 lab"
 
   ingress {
@@ -45,12 +45,12 @@ resource "aws_instance" "ec2" {
   instance_type = "t3.micro"
 
  
-  security_groups = [aws_security_group.ec2_sg.name]
+  security_groups = [aws_security_group.ec2_sg1.name]
 
   associate_public_ip_address = true
 
   tags = {
-    Name        = "terraform-ec2-lab1"
+    Name        = "terraform-ec2-lab2"
     Environment = "lab"
   }
 }
